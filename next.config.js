a6f01817db9d77c27676979da1876df1d5bd4189
Moses Ayo <legendmoses360@yahoo.com>
2022-10-25 06:07:30 +0100

    Fixed dashboard issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
+const path = require('path');
+const webpack = require("webpack");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
+
+module.exports ={ 
+  webpack(config, options) {
+  config.module.rules.push({
+    test: /\.(eot|ttf|woff|woff2)$/,
+    use: {
+      loader: "url-loader",
+    },
+  });
+  config.resolve.modules.push(path.resolve("./"));
+  return config;
+}};
